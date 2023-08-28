ebcf3099f31273bc9a2cc1e97d4ec40ab72de6a0
Fernando Mejorado Tinoco <fermejorado1@gmail.com>
2023-08-28 09:13:13 -0600

    Added game ended check and board reset functionality after

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -37,15 +37,46 @@ class ViewController: UIViewController {
     var NOUGHT = "O"
     var CROSS = "X"
     
+    //Board
+    var board = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initBoard()
+    }
+    
+    func initBoard(){
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
+        
     }
 
     @IBAction func boardTapAction(_ sender: UIButton) {
         addToBoard(sender)
+        
+        if(isBoardFull()){
+            resultAlert(title: "Draw")
+        }
     }
     
+    //Checks each space of the grid to check if its already full
+    func isBoardFull() -> Bool {
+        for button in board{
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
+    }
+    
+    //Adds the move to the board
     func addToBoard(_ sender: UIButton){
         if(sender.title(for: .normal) == nil){
             if(currentTurn == Turn.Cross){
@@ -57,7 +88,33 @@ class ViewController: UIViewController {
                 currentTurn = Turn.Cross
                 turnLabel.text = CROSS
             }
+            sender.isEnabled = false
         }
+    }
+    
+    //Shows results in alert box with reset button
+    func resultAlert(title: String){
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    //Clears up the board for a new match
+    func resetBoard(){
+        for button in board{
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if(firstTurn == Turn.Nought){
+            firstTurn = Turn.Cross
+            turnLabel.text = CROSS
+        } else {
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+        }
+        currentTurn = firstTurn
     }
     
 }
